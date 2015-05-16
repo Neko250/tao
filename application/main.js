43ae57aed51f934b0f144185e6adf5db71ed937b
Christopher Barnaby <cjbarnaby@gmail.com>
2015-05-17 09:03:20 +1000

    more html, style shell

---
+++ application/main.js
@@ -0,0 +1,12 @@
+var tocLevelOne = [
+  {book:"One", title:"The Silent Void",id:"one"},
+  {book:"Two", title:"The Ancient Masters",id:"two"},
+  {book:"Three", title:"Design",id:"three"},
+  {book:"Four", title:"Coding",id:"four"},
+  {book:"Five", title:"Maintenance",id:"five"},
+  {book:"Six", title:"Management",id:"six"},
+  {book:"Seven", title:"Corporate Wisdom",id:"seven"},
+  {book:"Eight", title:"Hardware and Software",id:"eight"},
+  {book:"Nine", title:"Epilogue",id:"nine"},
+];
+
