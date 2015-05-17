a5bbbf3bf565736abd549f218ffdd3bf2e351cb5
Christopher Barnaby <cjbarnaby@gmail.com>
2015-05-17 16:07:28 +1000

    toc styled

--- application/main.js
+++ application/main.js
@@ -1,13 +1,12 @@
 var tocLevelOne = [
-  {book:"One", title:"The Silent Void",id:"one"},
-  {book:"Two", title:"The Ancient Masters",id:"two"},
-  {book:"Three", title:"Design",id:"three"},
-  {book:"Four", title:"Coding",id:"four"},
-  {book:"Five", title:"Maintenance",id:"five"},
-  {book:"Six", title:"Management",id:"six"},
-  {book:"Seven", title:"Corporate Wisdom",id:"seven"},
-  {book:"Eight", title:"Hardware and Software",id:"eight"},
-  {book:"Nine", title:"Epilogue",id:"nine"},
+  {book:"One", title:"The Silent Void",toc:"tocOne", tao: "one"},
+  {book:"Two", title:"The Ancient Masters",toc:"tocTwo", tao: "two"},
+  {book:"Three", title:"Design",toc:"tocThree", tao: "three"},
+  {book:"Four", title:"Coding",toc:"tocFour", tao: "four"},
+  {book:"Five", title:"Maintenance",toc:"tocFive", tao: "five"},
+  {book:"Six", title:"Management",toc:"tocSix", tao: "six"},
+  {book:"Seven", title:"Corporate Wisdom",toc:"tocSeven", tao: "seven"},
+  {book:"Eight", title:"Hardware and Software",toc:"tocEight", tao: "eight"},
+  {book:"Nine", title:"Epilogue",toc:"tocNine", tao: "nine"},
 ];
 
-console.log
