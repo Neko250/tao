1209ab0949c33d7e9c6322b0da0d91ca1f8f3aa4
Christopher Barnaby <cjbarnaby@gmail.com>
2015-05-18 07:20:59 +1000

    fixed toc animation

--- application/main.js
+++ application/main.js
@@ -7,6 +7,39 @@ var tocLevelOne = [
   {book:"Six", title:"Management",toc:"tocSix", tao: "six"},
   {book:"Seven", title:"Corporate Wisdom",toc:"tocSeven", tao: "seven"},
   {book:"Eight", title:"Hardware and Software",toc:"tocEight", tao: "eight"},
-  {book:"Nine", title:"Epilogue",toc:"tocNine", tao: "nine"},
+  {book:"Nine", title:"Epilogue",toc:"tocNine", tao: "nine"}
 ];
+
+
+$(document).ready(function () {  
+
+  $('.theTao').hide();
+  $("#iconPlus").hide();
+  $(".startPage").stop().fadeOut(5000, function() {
+    })
+
+
+  $("#iconMinus").on("click", function() {
+    $(".tocWrapper").stop().animate({
+      width: "10px"
+    }, 200, function() {
+      $("#iconMinus").hide(); 
+      $("#iconPlus").show();   
+    });
+  });
+
+
+  $("#iconPlus").on("click", function() {
+    $(".tocWrapper").stop().animate({
+      width: "272px"
+    }, 200, function() {
+      $("#iconPlus").hide(); 
+      $("#iconMinus").show();   
+    });
+  });
+
+
+});
+
+
 
