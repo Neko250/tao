b5f146d6d55d2ff8bb3d826d1945d2ad5fbc5437
Christopher Barnaby <cjbarnaby@gmail.com>
2015-05-19 15:40:57 +1000

    Random and all TOC links working

--- application/main.js
+++ application/main.js
@@ -10,6 +10,7 @@ var tocLevelOne = [
   {book:"Nine", title:"Epilogue",toc:"tocNine", tao: "nine"}
 ];
 
+
 $(document).ready(function () {  
 
   $('.book').hide();
@@ -18,11 +19,12 @@ $(document).ready(function () {
   });
 
   $("#iconMinus").on("click", function() {    //Menu collapse on click
-    $(".tocWrapper").stop().animate({
-      width: "10px"
-    }, 400, function() {
       $(".tocLevelOne").hide();
       $(".toc > hr").hide();
+      $(".tocWrapper").stop().animate({
+      width: "10px"
+    }, 400, function() {
+
       $("#iconMinus").hide(); 
       $("#iconPlus").show();   
     });
@@ -38,7 +40,6 @@ $(document).ready(function () {
   });
 
 
-
   $("#iconPlus").on("click", function() {     //Menu expand on click
     $(".tocWrapper").stop().animate({
       width: "280px"
@@ -50,6 +51,30 @@ $(document).ready(function () {
     });
   });
 
+  var randomTaoGenerator = function() {
+    randomNumber = Math.random() * 10;
+    console.log(randomNumber)
+    if (randomNumber <= 2) {
+      randomTao = "one"
+    } else if ((randomNumber > 2) && (randomNumber <=3)) {
+      randomTao = "two"
+    } else if ((randomNumber > 3) && (randomNumber <=4)) {
+      randomTao = "three"
+    } else if ((randomNumber > 4) && (randomNumber <=5)) {
+      randomTao = "four"
+    } else if ((randomNumber > 5) && (randomNumber <=6)) {
+      randomTao = "five"
+    } else if ((randomNumber > 6) && (randomNumber <=7)) {
+      randomTao = "six"
+    } else if ((randomNumber > 7) && (randomNumber <=8)) {
+      randomTao = "seven"
+    } else if ((randomNumber > 8) && (randomNumber <=9)) {
+      randomTao = "six"
+    } else if ((randomNumber > 9) && (randomNumber <=10)) {
+      randomTao = "six"
+    } return randomTao;
+  };
+
   var getPage = function() {
 
     $(".tocLevelOne").on("click", function(){
@@ -59,10 +84,18 @@ $(document).ready(function () {
       var x = $(this).attr('id');
       x = x.substring(3);
       x = x.toLowerCase();
+      console.log(x)
       if (x === "all") {
-        $(".theTao .book").show();
+        $(".theTao .book").fadeIn(2000, function() {
+        });
+      } else if (x === "randomtao") {
+        randomTaoGenerator()
+        $("#" + randomTao).fadeIn(2000, function() {
+        });
+    
       } else {
         $("#" + x).fadeIn(2000, function() {
+        console.log(x)
         });
       }
     });
